9590db39e5e22b0563b038f422ecc7f9487dae1c
Fede <fede@fedehartman.com>
2012-10-15 16:33:24 -0200

    Versión 1.00
    Miércoles 15 de Octube de 2012

---
+++ index.php
@@ -0,0 +1,92 @@
+<!doctype html>
+
+<!--
+Site Name: 
+Site URI: http://
+Version: 1.0
+Design Date: May XX - Aug XX | 2012
+Coding Date: Aug XX - Sep XX | 2012
+Design: XXX | http:// - @xxx
+HTML/CSS, jQuery: XXX | http:// - @xxx
+Some stuff made by Initializr | http://www.initializr.com/
+--> 
+
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+   <!--[if IE 7]> <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+   <!--[if IE 8]> <html class="no-js lt-ie9"> <![endif]-->
+
+<!--[if gt IE 8]><!-->
+<html class="no-js" lang="es-UY">
+<!--<![endif]-->
+
+<head>
+
+    <title></title>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="description" content="">
+    <meta name="author" content="Xxx | http://" />
+    <meta name="keywords" content="" /> 
+    <meta name="robots" content="all" /> 
+    <meta name="viewport" content="width=device-width">
+
+    <link rel="shortcut icon" href="img/favicons/favicon.ico" />
+    <link rel="apple-touch-icon" href="img/favicons/apple-touch-icon.png" />
+    <link rel="apple-touch-icon" sizes="57x57" href="img/favicons/apple-touch-icon-57x57.png" />
+    <link rel="apple-touch-icon" sizes="72x72" href="img/favicons/apple-touch-icon-72x72.png" />
+    <link rel="apple-touch-icon" sizes="114x114" href="img/favicons/apple-touch-icon-114x144.png" />
+    <link rel="apple-touch-icon" sizes="144x144" href="img/favicons/apple-touch-icon-144x144.png" />
+
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.css">
+
+    <!--[if lt IE 9]><script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+
+</head>
+
+<body>
+
+
+
+<!--| IE WARNING
+==================================================================================================== |-->
+
+    <!--[if lt IE 9]>
+        <p class="chromeframe">Estás usando un navegador viejo. <a href="http://browsehappy.com/">Actualizalo aquí</a> para ver correctamente el contenido de este y otros sitios</p>
+    <![endif]-->
+
+
+
+<!--| HEADER
+======================================================================================================================== |-->
+
+<header>
+
+</header>
+
+
+
+<!--| MAIN
+======================================================================================================================== |-->
+
+<div id="main">
+    Hola, qué tal.
+</div>
+
+
+
+<!--| FOOTER
+======================================================================================================================== |-->
+
+<footer>
+
+</footer>
+
+
+<!--| ================================================================================================================== |-->
+
+<?php include("scripts.php"); ?>
+
+</body>
+</html>
---
+++ scripts.php
@@ -0,0 +1,19 @@
+
+
+<!--| SCRIPTS
+======================================================================================================================== |-->
+
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js" type="text/javascript"></script>
+
+    <script src="js/plugins.js"></script>
+    <!--<script src="js/xxxxxx.js"></script>-->
+    <script src="js/main.js"></script>
+
+    <script>
+        var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+        (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+        g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+        s.parentNode.insertBefore(g,s)}(document,'script'));
+    </script>
+
+
