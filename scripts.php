431324a344ab58ad7665279dba1fb8d87f2210e8
Fede <fede@fedehartman.com>
2013-01-15 15:30:03 -0200

    Versión 1.08

--- scripts.php
+++ scripts.php
@@ -8,11 +8,3 @@
 	<script src="js/plugins.js"></script>
 	<script src="js/main.js"></script>
 
-	<script>
-		var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
-		(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-		g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
-		s.parentNode.insertBefore(g,s)}(document,'script'));
-	</script>
-
-
